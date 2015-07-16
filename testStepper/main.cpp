6a21d62a9f6d7abbca7f0d4502c846be6dabc883
Lucas Ribeiro de Abreu <Lucas@Lucass-MacBook-Pro.local>
2015-07-16 11:24:18 -0300

    Initial Commit

---
+++ testStepper/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testStepper
+//
+//  Created by Lucas Ribeiro de Abreu on 16/07/2015.
+//  Copyright (c) 2015 Lucas Ribeiro de Abreu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
